d60c553466c990e65d46cf72ab6769bee51acfe0
alk3pInjection <webmaster@raspii.tech>
2022-02-16 10:18:48 +0800

    Update for S
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- android/build.sh
+++ android/build.sh
@@ -6,15 +6,15 @@ fi
 
 echo "--- Syncing"
 
-cd /buildbot/lineage-18.1
+cd /buildkite/src
 rm -rf .repo/local_manifests/*
-repo init -u https://github.com/Project-1CE/manifest -b lineage-18.1 --depth 1
+repo init -u https://github.com/Project-1CE/manifest -b sugisawa --depth 1
 repo forall -vc "git clean -fdx; git checkout -f"
 repo sync -c -j$(nproc --all) --force-sync --fail-fast --no-clone-bundle --no-tags || exit 1
 
 echo "--- Cleaning"
 
-rm -rf out/target/product
+rm -rf out/target/product kernel/*/ship_prebuilt
 grep -q $DEVICE out/last_device
 if [ $? -gt 0 ]; then
     rm -rf out/
@@ -23,24 +23,22 @@ fi
 echo "--- Building"
 
 . build/envsetup.sh
-lunch lineage_${DEVICE}-userdebug || exit 1
+lunch ice_${DEVICE}-userdebug || exit 1
 echo ${DEVICE} > out/last_device
 
 echo "--- Building"
 mka installclean
-mka bacon -j$(nproc --all) || exit 1
+mka bacon || exit 1
 
 echo "--- Uploading"
 
-scp out/target/product/${DEVICE}/lineage-18.1-*.zip blob.project-ice.org:/jenkins/incoming/
-
-meta_filename=$(ls out/target/product/${DEVICE}/lineage-18.1-*.zip | sed 's#.*/##')
+meta_filename=$(ls out/target/product/${DEVICE}/Ice-sugisawa-*.zip | sed 's#.*/##')
 meta_incr=$(cat out/target/product/${DEVICE}/ota_metadata | grep post-build-incremental | cut -c 24-)
-meta_device=$DEVICE
-meta_version="18.1"
-meta_romtype="unofficial"
-meta_sha256sum=$(sha256sum out/target/product/${DEVICE}/lineage-18.1-*.zip | awk '{ print $1 }')
-meta_romsize=$(ls -la out/target/product/${DEVICE}/lineage-18.1-*.zip | awk '{ print $5 }')
+meta_device=${DEVICE}
+meta_version="sugisawa"
+meta_romtype="nightly"
+meta_sha256sum=$(cat out/target/product/${DEVICE}/${meta_filename}.sha256sum | awk '{ print $1 }')
+meta_romsize=$(ls -la out/target/product/${DEVICE}/${meta_filename} | awk '{ print $5 }')
 meta_url="https://otafsg-cost.project-ice.org/full/"${meta_device}"/"${meta_filename}
 meta_datetime=$(cat out/target/product/${DEVICE}/ota_metadata | grep post-timestamp | cut -c 16-)
 
